ab8c01dcc054a8d6f66642af517a96f98cd242b6
Alex Zúñiga S <40903825+alexzu02@users.noreply.github.com>
2023-08-16 20:47:53 -0600

    Password Generator

--- Password Generator Project.py
+++ Password Generator Project.py
@@ -1,3 +1,5 @@
+
+
 Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> #Password Generator Project
@@ -30,4 +32,5 @@ Type "help", "copyright", "credits" or "license()" for more information.
 ... password = ""
 ... for char in password_list:
 ...   password += char
-... 
+
+... print(f"Su contraseña es: {password}")
